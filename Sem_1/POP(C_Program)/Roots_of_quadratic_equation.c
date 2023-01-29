7c18f63e8cc88e570d6249ba9e7dc7f01a573582
AshishRamesh <81244883+AshishRamesh@users.noreply.github.com>
2023-01-29 16:27:31 +0530

    Create Roots_of_quadratic_equation.c

---
+++ Sem_1/POP(C_Program)/Roots_of_quadratic_equation.c
@@ -0,0 +1,35 @@
+#include <stdio.h> 
+#include <math.h>
+
+int main()
+
+{
+    float a,b,c,dis,r1,r2, realpart, imgpart; 
+    
+    printf("Enter the co-efficients : \n"); 
+    scanf("%f %f %f",&a, &b, &c); 
+    dis = (b*b)-4*a*c;
+
+    if (dis>0)
+    {
+        printf("The roots are real and distinct \n"); 
+        r1 = (-b+sqrt(dis))/(2*a);
+        r2 = (-b-sqrt(dis))/(2*a);
+        printf("The roots are r1 = %f, r2 = %f \n",r1,r2);
+    }
+    
+    else if (dis==0)
+    {
+        r1=r2=-b/(2*a);
+        printf("The roots are equal \n");
+    }
+
+    else
+    { 
+        realpart = -b/(2*a); 
+        imgpart = sqrt(-dis)/(a*a); 
+        printf ("The roots are imaginary \n");
+        return 0;
+    }
+
+}
